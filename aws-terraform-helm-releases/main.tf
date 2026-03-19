6cd4a1e8505c698a6cb605345ec7059bc09f4382
amadinathaniel <amadinathaniel@gmail.com>
2026-03-19 09:10:50 +0000

    add values flag for force delete

--- aws-terraform-helm-releases/main.tf
+++ aws-terraform-helm-releases/main.tf
@@ -400,6 +400,10 @@ resource "helm_release" "external_secrets" {
     {
       name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
       value = aws_iam_role.external_secrets[0].arn
+    },
+    {
+      name  = "webhook.failurePolicy"
+      value = "Ignore"
     }
   ]
 
